0b6c71d2b7297929e29e5fd03f8523b8c76ed6d0
Safia Abdalla <safia@safia.rocks>
2016-12-07 21:12:23 -0600

    test: add tests for FileViewer component

--- src/tests/file-viewer.spec.js
+++ src/tests/file-viewer.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'enzyme';
 import FileViewer from '../components/file-viewer';
 
 it('renders without crashing', () => {
@@ -9,4 +10,15 @@ it('renders without crashing', () => {
     { name: 'test.png', size: '1024', lastModified: new Date().toString() },
   ];
   ReactDOM.render(<FileViewer data={testData} />, div);
+});
+
+it('renders a table with 3 rows', () => {
+  const testData = [
+    { name: 'test.png', size: '512', lastModified: new Date().toString() },
+    { name: 'test.png', size: '1024', lastModified: new Date().toString() },
+  ];
+  const component = render(
+    <FileViewer data={testData}/>
+  );
+  expect(component.find('tr').length).toBe(3);
 });
