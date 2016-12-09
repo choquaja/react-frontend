bee2be74e5598f4b398169e49e15ac6aeca5937a
Safia Abdalla <safia@safia.rocks>
2016-12-09 14:53:04 -0600

    chore: add classnames to table columns

--- src/components/file-viewer.js
+++ src/components/file-viewer.js
@@ -2,14 +2,42 @@ import Table from 'rc-table';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class FileViewerWrapper extends React.Component {
+  sortData(key) {
+    this.props.data.sort((a, b) => {
+      return parseFloat(a[key]) - parseFloat(b[key]);
+    });
+  }
+
+  onClick(event, target) {
+    const element = ReactDOM.findDOMNode(event.target);
+    if (element.tagName === 'TH') {
+      const elementClass = element.className;
+      console.log(this);
+      this.sortData(elementClass);
+    }
+  }
+
+  render() {
+    return (
+      <div onClick={this.onClick}>
+        <FileViewer
+          data={this.props.data}
+        />
+      </div>
+    );
+  }
+}
+
 class FileViewer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       columns: [
-        { title: 'Name', dataIndex: 'name', key: 'name' },
-        { title: 'Size', dataIndex: 'size', key: 'size' },
-        { title: 'Last Modified', dataIndex: 'lastModified', key: 'lastModified' },
+        { title: 'Name', dataIndex: 'name', key: 'name', className: 'name' },
+        { title: 'Size', dataIndex: 'size', key: 'size', className: 'size' },
+        { title: 'Last Modified', dataIndex: 'lastModified',
+          key: 'lastModified', className: 'lastModified' },
       ]
     };
   }
