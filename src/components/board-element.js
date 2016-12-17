29a49bb6ea330d5c6b083fe94baf7a8e8467bd11
Safia Abdalla <safia@safia.rocks>
2016-12-16 19:41:04 -0700

    feature: add board element component

---
+++ src/components/board-element.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class BoardElement extends React.Component {
+  render() {
+    return (
+      <div className="board-element-element" id={this.props.element.id}>
+        <div className="board-element-name">
+          {this.props.element.title}
+        </div>
+        <div className="board-element-content">
+          {this.props.element.content}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BoardElement;
