a5ca8c9be0757b061b14defa47c1a4df3a8b3857
Safia Abdalla <safia@safia.rocks>
2016-12-16 19:39:32 -0700

    feature: update main render

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FileViewerWrapper from './components/file-viewer';
+import Board from './components/board';
 import './stylesheets/style.css';
 
-const testData = [
-  { name: 'test.png', size: '512', lastModified: new Date().toString() },
-  { name: 'test.png', size: '1024', lastModified: new Date().toString() },
+const boards = [
+  {
+    id: 1,
+    name: 'Board 1',
+    elements: [
+      { key: 1, id: 1, title: 'Title 1', content: 'Content 1' },
+      { key: 2, id: 2, title: 'Title 2', content: 'Content 2' },
+      { key: 3, id: 3, title: 'Title 3', content: 'Content 3' }
+  ]},
+  {
+    id: 2,
+    name: 'Board 2',
+    elements: [
+      { key: 4, id: 4, title: 'Title 4', content: 'Content 4' },
+      { key: 5, id: 5, title: 'Title 5', content: 'Content 5' },
+      { key: 6, id: 6, title: 'Title 6', content: 'Content 6' }
+    ]
+  },
 ];
 
 ReactDOM.render(
-  <FileViewerWrapper data={testData}/>,
+  <Board
+    key={'test'}
+    boards={boards}
+  />,
   document.getElementById('root')
 );
