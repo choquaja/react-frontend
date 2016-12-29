86d7b830493af0ede611f2f144ca06c1538acdff
Safia Abdalla <safia@safia.rocks>
2016-12-28 21:16:29 -0800

    feature: update homepage with layout

--- src/index.js
+++ src/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Board from './components/board';
 import './stylesheets/style.css';
-
-const boards = [
-  {
-    id: 1,
-    name: 'Board 1',
-    elements: [
-      { key: 1, id: 1, title: 'Title 1', content: 'Content 1' },
-      { key: 2, id: 2, title: 'Title 2', content: 'Content 2' },
-      { key: 3, id: 3, title: 'Title 3', content: 'Content 3' }
-  ]},
-  {
-    id: 2,
-    name: 'Board 2',
-    elements: [
-      { key: 4, id: 4, title: 'Title 4', content: 'Content 4' },
-      { key: 5, id: 5, title: 'Title 5', content: 'Content 5' },
-      { key: 6, id: 6, title: 'Title 6', content: 'Content 6' }
-    ]
-  },
-];
+import Header from './components/layout/header';
+import VerticalNavigation from './components/layout/vertical-navigation';
 
 ReactDOM.render(
-  <Board
-    key={'test'}
-    boards={boards}
-  />,
-  document.getElementById('root')
-);
+  <main>
+    <div className="header">
+      <Header/>
+    </div>
+    <div className="sidebar">
+      <VerticalNavigation/>
+    </div>
+    <div class="main">
+    </div>
+    <footer>
+    </footer>
+  </main>,
+document.getElementById('root'));
