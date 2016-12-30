659a817c48e74c0ee08400a2a7e4aefc2957964f
Safia Abdalla <safia@safia.rocks>
2016-12-30 09:30:13 -0800

    feature: add route

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+import App from './components/routes/app';
+import Project from './components/routes/project';
+import Resources from './components/routes/resources';
+import Workspaces from './components/routes/workspaces';
+import Models from './components/routes/models';
+import Jobs from './components/routes/jobs';
+import Collaborators from './components/routes/collaborators';
+import ProjectSettings from './components/routes/project-settings';
+
 import './stylesheets/style.css';
-import Header from './components/layout/header';
-import ProjectHeader from './components/layout/project-header';
-import VerticalNavigation from './components/layout/vertical-navigation';
 
 ReactDOM.render(
-  <main>
-    <div className="header">
-      <Header/>
-    </div>
-    <div className="sidebar">
-      <VerticalNavigation/>
-    </div>
-    <div className="main">
-      <ProjectHeader 
-        namespace={'testUser'}
-        projectName={'testProject'}
-        location={'testLocation'}
-        locationName={'testLocationName'}/>
-    </div>
-    <footer>
-    </footer>
-  </main>,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="/:userName/projects/:projectId" component={Project}>
+        <Route path="/:userName/projects/:projectId/resources" component={Resources}>
+        </Route>
+        <Route path="/:userName/projects/:projectId/workspaces" component={Workspaces}>
+        </Route>
+        <Route path="/:userName/projects/:projectId/models" component={Models}>
+        </Route>
+        <Route path="/:userName/projects/:projectId/jobs" component={Jobs}>
+        </Route>
+        <Route path="/:userName/projects/:projectId/collaborators" component={Collaborators}>
+        </Route>
+        <Route path="/:userName/projects/:projectId/settings" component={ProjectSettings}>
+        </Route>
+      </Route>
+    </Route>
+  </Router>,
 document.getElementById('root'));
