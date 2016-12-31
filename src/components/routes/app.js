d26a96f0250f9afaa3494bd3789a0da5d091499c
Safia Abdalla <safia@safia.rocks>
2016-12-30 16:16:13 -0800

    feature: add application footer

--- src/components/routes/app.js
+++ src/components/routes/app.js
@@ -6,8 +6,19 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
+        <div className="header">
+          <Header/>
+        </div>
         {this.props.children}
+        <footer>
+          <div class="container">
+            &copy; 2016 3Blades. LLC
+            <span className="footer-separator"> |</span>
+            <a href="/terms-of-use/">Terms of Use</a>
+            <span className="footer-separator">|</span>
+            <a href="/privacy-policy/">Privacy Policy</a>
+          </div>
+        </footer>
       </div>
     );
   }
