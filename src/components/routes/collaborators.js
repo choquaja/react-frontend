8ca6250401aabdd18dfc47a61709beee24fb0e94
Safia Abdalla <safia@safia.rocks>
2016-12-30 20:07:47 -0800

    feature: add Collaborators component

--- src/components/routes/collaborators.js
+++ src/components/routes/collaborators.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Table from 'rc-table';
+import * as Material from 'react-icons/lib/md';
 
 class Collaborators extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columns: [
+        { title: 'Name', dataIndex: 'name', key: 'name', className: 'name' },
+        { title: 'Role', dataIndex: 'role', key: 'role', className: 'role' }
+      ],
+      data: []
+    };
+  }
+
   render() {
     return (
-      <h1> This is a collaborators test </h1>
+      <div>
+        <div className="table-toolbar">
+          <Material.MdAddCircleOutline/>
+        </div>
+        <Table
+          columns={this.state.columns}
+          data={this.state.data}
+        />
+      </div>
     );
   }
 }
