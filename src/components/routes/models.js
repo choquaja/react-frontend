001584640ebf554ebf07684e87707500564db1dc
Safia Abdalla <safia@safia.rocks>
2016-12-31 01:11:20 -0800

    fix: refactor Models component

--- src/components/routes/models.js
+++ src/components/routes/models.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import ResourceTable from '../tables/resource-table';
+import * as Material from 'react-icons/lib/md';
+import { Link } from 'react-router';
 
 class Models extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    const that = this;
+    fetch().then(function(response) {
+      if (response.ok) {
+        that.setState({
+          data: response
+        });
+      }
+    });
+  }
+
   render() {
+    const { userName, projectId } = this.props.params;
     return (
-      <ResourceTable/>
+      <div>
+        <div className="table-toolbar">
+          <Material.MdPlayArrow/>
+          <Material.MdPause/>
+          <Material.MdDeleteForever/>
+          <Link to={`${userName}/projects/${projectId}/models/new`}>
+            <Material.MdAddCircleOutline/>
+          </Link>
+        </div>
+        <ResourceTable serverType={"models"}/>
+      </div>
     );
   }
 }
