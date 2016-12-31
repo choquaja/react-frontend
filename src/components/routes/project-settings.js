17bbc473caa9a5e91bf9b6d3fe514a8112f652d7
Safia Abdalla <safia@safia.rocks>
2016-12-30 20:33:36 -0800

    feature: add ProjectSettings component

--- src/components/routes/project-settings.js
+++ src/components/routes/project-settings.js
@@ -3,7 +3,23 @@ import React from 'react';
 class ProjectSettings extends React.Component {
   render() {
     return (
-      <h1> This is a project settings test </h1>
+      <div>
+        <form>
+          <div className="form-group">
+            <label for="projectName">Project Name</label>
+            <input type="text" className="form-control" id="projectName"/>
+          </div>
+          <div className="form-group">
+            <label for="projectDesc">Project Description</label>
+            <textarea className="form-control" id="projectDesc"/>
+          </div>
+          <div className="settings-buttons">
+            <button className="private-project">Make Project Private</button>
+            <button className="delete-project">Delete Project</button>
+            <button className="save" type="submit">Save</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
