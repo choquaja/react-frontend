1f72573659103c21af4a99a37706070827edb6b7
Safia Abdalla <safia@safia.rocks>
2016-12-30 17:41:01 -0800

    feature: add icons to vertical nav

--- src/components/layout/vertical-navigation.js
+++ src/components/layout/vertical-navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../images/icon-sprite.svg';
+import * as Material from 'react-icons/lib/md';
 import { Link } from 'react-router';
 
 class VerticalNavigation extends React.Component {
@@ -9,27 +9,35 @@ class VerticalNavigation extends React.Component {
       <div className="vertical-nav">
         <ul className="vertical-nav-menu">
           <li>
+            <Material.MdRemoveRedEye/>
             <Link to={`/${userName}/projects/${projectId}`}>Overview</Link>
           </li>
           <li>
+            <Material.MdDashboard/>
             <Link to={`/${userName}/projects/${projectId}/board`}>Board</Link>
           </li>
           <li>
+            <Material.MdFolder/>
             <Link to={`/${userName}/projects/${projectId}/resources`}>Files</Link>
           </li>
           <li>
+            <Material.MdWork/>
             <Link to={`/${userName}/projects/${projectId}/workspaces`}>Workspaces</Link>
           </li>
           <li>
+            <Material.MdFunctions/>
             <Link to={`/${userName}/projects/${projectId}/models`}>Models</Link>
           </li>
           <li>
+            <Material.MdBuild/>
             <Link to={`/${userName}/projects/${projectId}/jobs`}>Jobs</Link>
           </li>
           <li>
+            <Material.MdPeople/>
             <Link to={`/${userName}/projects/${projectId}/collaborators`}>Collaborators</Link>
           </li>
           <li>
+            <Material.MdSettings/>
             <Link to={`/${userName}/projects/${projectId}/settings`}>Settings</Link>
           </li>
         </ul>
