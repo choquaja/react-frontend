2f1830aed6a0d941a31e094ea3494499e4da4b5e
Safia Abdalla <safia@safia.rocks>
2017-01-02 08:40:51 -0600

    feature: add project state reducers

--- src/reducers/project.js
+++ src/reducers/project.js
@@ -4,7 +4,16 @@ import * as constants from '../actions/constants';
 export default handleActions({
   [constants.RENAME_PROJECT]: function(state, action) {
     const { newName } = action;
-    state.name = newName;
-    return state;
+    return state.set('name', newName);
+  },
+  [constants.CHANGE_DESCRIPTION]: function(state, action) {
+    const { newDescription } = action;
+    return state.set('description', newDescription);
+  },
+  [constants.TOGGLE_VISIBILITY]: function(state, action) {
+    return state.set('isPivate', !state.get('isPrivate'));
+  },
+  [constants.DELETE_PROJECT]: function(state, action) {
+    return null;
   }
 }, {});
