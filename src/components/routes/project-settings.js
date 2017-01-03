e87feee86f5085ff96a87e981a06d65a3f8d4e1b
Safia Abdalla <safia@safia.rocks>
2017-01-03 09:50:05 -0600

    feature: attach redux to project settings component

--- src/components/routes/project-settings.js
+++ src/components/routes/project-settings.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {
+  renameProject,
+  changeDescription,
+  toggleVisibility,
+  deleteProject,
+} from '../../actions';
 
 class ProjectSettings extends React.Component {
   render() {
+    const { onToggleVisibility, onDeleteProject, onSave } = this.props;
     return (
       <div>
         <form>
           <div className="form-group">
-            <label for="projectName">Project Name</label>
-            <input type="text" className="form-control" id="projectName"/>
+            <label htmlFor="projectName">Project Name</label>
+            <input type="text" className="form-control" id="projectName" placeholder={this.props.name}/>
           </div>
           <div className="form-group">
-            <label for="projectDesc">Project Description</label>
-            <textarea className="form-control" id="projectDesc"/>
+            <label htmlFor="projectDesc">Project Description</label>
+            <textarea className="form-control" id="projectDesc" placeholder={this.props.description}/>
           </div>
           <div className="settings-buttons">
-            <button className="private-project">Make Project Private</button>
-            <button className="delete-project">Delete Project</button>
-            <button className="save" type="submit">Save</button>
+            <button className="private-project" onClick={(event) => onToggleVisibility(event)}>
+              Make Project { this.props.isPrivate ? 'Public' : 'Private' }
+            </button>
+            <button className="delete-project" onClick={() => onDeleteProject()}>Delete Project</button>
+            <button className="save" type="submit" onClick={() => onSave()}>Save</button>
           </div>
         </form>
       </div>
@@ -24,4 +34,22 @@ class ProjectSettings extends React.Component {
   }
 }
 
-export default ProjectSettings;
+export const mapStateToProps = (state) => ({
+  name: state.project.get('name'),
+  description: state.project.get('description'),
+  isPrivate: state.project.get('isPrivate')
+});
+
+export const mapDispatchToProps = (dispatch) => ({
+  onSave: (newName, newDescription) => {
+    if (newName) dispatch(renameProject(newName));
+    if (newDescription) dispatch(changeDescription(newDescription));
+  },
+  onToggleVisibility: (event) => {
+    event.preventDefault();
+    dispatch(toggleVisibility());
+  },
+  onDeleteProject: () => dispatch(deleteProject()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectSettings);
