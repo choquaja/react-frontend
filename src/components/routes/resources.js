3feb6779a29bfd699383debf9baf1605f9ea602c
Safia Abdalla <safia@safia.rocks>
2017-01-03 10:24:08 -0600

    feature: refactor resource component for single view

--- src/components/routes/resources.js
+++ src/components/routes/resources.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ResourceTable from '../tables/resource-table';
+import Table from 'rc-table';
 import * as Material from 'react-icons/lib/md';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
@@ -8,41 +8,42 @@ class Workspaces extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
-    }
-  }
-
-  componentDidMount() {
-    const that = this;
-    fetch().then(function(response) {
-      if (response.ok) {
-        that.setState({
-          data: response,
-        });
-      }
-    });
+      columns: [
+        { title: 'Name', dataIndex: 'name', key: 'name', className: 'name' },
+        { title: 'Status', dataIndex: 'status', key: 'status', className: 'status' },
+        { title: 'Type', dataIndex: 'type', key: 'type', className: 'type' },
+        { title: 'Resources', dataIndex: 'resources', key: 'resources', className: 'resources' },
+      ],
+    };
   }
 
   render() {
     const { userName, projectId } = this.props.params;
+    const { workspaces, models, jobs } = this.props;
+    const data = workspaces.concat(models, jobs);
     return (
       <div>
         <div className="table-toolbar">
           <Material.MdPlayArrow/>
           <Material.MdPause/>
           <Material.MdDeleteForever/>
-          <Link to={`${userName}/projects/${projectId}/workspaces/new`}>
+          <Link to={`${userName}/projects/${projectId}/resources/new`}>
             <Material.MdAddCircleOutline/>
           </Link>
         </div>
-        <ResourceTable serverType={"workspaces"} data={this.props.data}/>
+        <Table
+          columns={this.state.columns}
+          data={data.toJS()}
+        />
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  data: state.workspaces,
+  workspaces: state.workspaces,
+  models: state.models,
+  jobs: state.jobs,
 });
 
 export default connect(mapStateToProps)(Workspaces);
