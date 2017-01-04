0f3894ea6170b69f7b6fdf099b13c6d7db6f88b6
Safia Abdalla <safia@safia.rocks>
2017-01-04 13:27:07 -0600

    feature: toggle selected row in table

--- src/components/tables/table.js
+++ src/components/tables/table.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onRowClick = this.onRowClick.bind(this);
+  }
+
+  onRowClick(event, target) {
+    const currentlySelected = document.getElementsByClassName('selected');
+    if (currentlySelected.length === 1) {
+      currentlySelected.item(0).classList.toggle('selected');
+    }
+    const row = event.target.parentNode;
+    row.classList.add('selected');
+  }
+
   render() {
     const { columns, data } = this.props;
     return (
@@ -9,17 +23,21 @@ class Table extends React.Component {
           <tr>
             {
               columns.map((column) => {
-                return <th>{column.title}</th>;
+                return <th key={column.title}>{column.title}</th>;
               })
             }
           </tr>
           {
-            data.map((datum) => {
+            data.map((datum, index) => {
               return (
-                <tr>
+                <tr key={index} onClick={this.onRowClick}>
                   {
                     columns.map((column) => {
-                      return <td>{datum.getIn(column.key)}</td>;
+                      return <td
+                              key={column.className}
+                              onClick={this.onRowClick}>
+                                {datum.getIn(column.key)}
+                            </td>;
                     })
                   }
                 </tr>
