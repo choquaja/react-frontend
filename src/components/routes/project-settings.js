c728090a672683a71e6a8d496959661b81ca635a
Safia Abdalla <safia@safia.rocks>
2017-01-06 16:35:09 -0600

    feature: add ability to modify project

--- src/components/routes/project-settings.js
+++ src/components/routes/project-settings.js
@@ -8,26 +8,57 @@ import {
 } from '../../actions';
 
 class ProjectSettings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.state = {
+      'projectName': '',
+      'projectDesc': '',
+    }
+  }
+
+  onChange(event) {
+    event.preventDefault();
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  }
+
   render() {
     const { onToggleVisibility, onDeleteProject, onSave } = this.props;
-    console.log(this.props.project.get('isPrivate'));
     return (
       <div>
         <form>
           <div className="form-group">
             <label htmlFor="projectName">Project Name</label>
-            <input type="text" className="form-control" id="projectName" placeholder={this.props.project.get('name')}/>
+            <input
+              type="text"
+              className="form-control"
+              id="projectName"
+              placeholder={this.props.project.get('name')}
+              onChange={this.onChange}/>
           </div>
           <div className="form-group">
             <label htmlFor="projectDesc">Project Description</label>
-            <textarea className="form-control" id="projectDesc" placeholder={this.props.project.get('description')}/>
+            <textarea
+              className="form-control"
+              id="projectDesc"
+              placeholder={this.props.project.get('description')}
+              onChange={this.onChange}/>
           </div>
           <div className="settings-buttons">
             <button className="private-project" onClick={(event) => onToggleVisibility(event)}>
               Make Project { this.props.project.get('isPrivate') ? 'Public' : 'Private' }
             </button>
             <button className="delete-project" onClick={() => onDeleteProject()}>Delete Project</button>
-            <button className="save" type="submit" onClick={() => onSave()}>Save</button>
+            <button
+              className="save"
+              type="submit"
+              onClick={(event) => {
+                event.preventDefault();
+                onSave(this.state.projectName, this.state.projectDesc); } }>
+                Save
+            </button>
           </div>
         </form>
       </div>
@@ -41,6 +72,7 @@ export const mapStateToProps = (state) => ({
 
 export const mapDispatchToProps = (dispatch) => ({
   onSave: (newName, newDescription) => {
+    event.preventDefault();
     if (newName) dispatch(renameProject(newName));
     if (newDescription) dispatch(changeDescription(newDescription));
   },
