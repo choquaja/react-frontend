1b3b535aa1bec41e60ea3bd2f9928a220ac3c571
Safia Abdalla <safia@safia.rocks>
2017-01-13 05:52:33 -0600

    test: update NavigationLink tests

--- src/tests/components/layout/navigation-link.spec.js
+++ src/tests/components/layout/navigation-link.spec.js
@@ -1,4 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import NavigationLink from '../../../components/layout/navigation-link';
 
-
+it('renders without crashing', () => {
+  const props = {
+    index: false,
+    onlyActiveOnIndex: false,
+    to: 'test/location',
+    IconComponent: 'span',
+  };
+  const component = mount(
+    <NavigationLink {...props}/>, {
+      context: {
+        router : {
+          isActive: (to, onlyActiveOnIndex) => { return true; },
+          push: () => { },
+          replace: () => { },
+          go: () => { },
+          goBack: () => { },
+          goForward: () => { },
+          setRouteLeaveHook: () => { },
+          createHref: () => { },
+        } 
+      }
+    }
+  );
+});
