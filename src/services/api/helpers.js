2f5bd42e172e2ed562026238ef2001e6ce0e76db
Ray Gesualdo <ray@rjgesualdo.com>
2017-05-22 14:15:10 -0400

    refactored api to gracefully handle invalid JWT errors
    Signed-off-by: Ray Gesualdo <ray@rjgesualdo.com>

--- src/services/api/helpers.js
+++ src/services/api/helpers.js
@@ -1,5 +1,6 @@
 import getOr from 'lodash/fp/getOr';
 import { SubmissionError } from 'redux-form';
+import { logout } from '../authToken';
 
 const getApiErrors = getOr({})('response.data');
 export const mapErrorResponseToReduxForm = (error) => {
@@ -9,4 +10,16 @@ export const mapErrorResponseToReduxForm = (error) => {
     delete apiErrors.non_field_errors;
   }
   return new SubmissionError(apiErrors);
+};
+
+const getStatus = getOr(0)('response.status');
+export const redirectIfAuthInvalid = ({ history }) => (error) => {
+  if (getStatus(error) === 401) {
+    logout();
+    history.push({
+      location: '/auth/login',
+      state: { next: history.location.pathname },
+    });
+  }
+  return error;
 };
