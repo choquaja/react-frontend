ec2c0f64bb26bb6d472f608fb8f9bbf4abe9dea2
Ray Gesualdo <ray@rjgesualdo.com>
2017-06-02 17:43:28 -0400

    removed old reducer files
    Signed-off-by: Ray Gesualdo <ray@rjgesualdo.com>

--- src/scenes/Project/scenes/Files/types.js
+++
@@ -1,4 +0,0 @@
-export const DELETE_FILE = 'project.files.DELETE_FILE';
-export const EDIT_FILE = 'project.files.EDIT_FILE';
-export const RENAME_FILE = 'project.files.RENAME_FILE';
-export const DUPLICATE_FILE = 'project.files.DUPLICATE_FILE';
--- src/scenes/Project/scenes/Settings/types.js
+++
@@ -1,4 +0,0 @@
-export const RENAME_PROJECT = 'project.settings.RENAME_PROJECT';
-export const CHANGE_DESCRIPTION = 'project.settings.CHANGE_DESCRIPTION';
-export const TOGGLE_VISIBILITY = 'project.settings.TOGGLE_VISIBILITY';
-export const DELETE_PROJECT = 'project.settings.DELETE_PROJECT';
