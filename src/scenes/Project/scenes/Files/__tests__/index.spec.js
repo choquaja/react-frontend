15f62f75a2abba9c5e423280b4b960a9e5f8cc36
Ray Gesualdo <ray@rjgesualdo.com>
2017-06-02 17:38:15 -0400

    removed obsolete tests
    Signed-off-by: Ray Gesualdo <ray@rjgesualdo.com>

--- src/scenes/Project/scenes/Files/__tests__/index.spec.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { Files } from '../';
-import files from './initialState';
-
-it('renders without crashing', () => {
-  shallow(<Files data={files} />);
-});
