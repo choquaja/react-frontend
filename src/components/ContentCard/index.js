23efdeae0d6daa0c0ab748ae8dd759cfd19dfbdb
Ray Gesualdo <ray@rjgesualdo.com>
2017-06-03 17:20:11 -0400

    added styling to ContentCard
    Signed-off-by: Ray Gesualdo <ray@rjgesualdo.com>

--- src/components/ContentCard/index.js
+++ src/components/ContentCard/index.js
@@ -5,17 +5,24 @@ const isColumn = props => props.column && css`
   margin: 0 -1rem;
 `;
 
+const isTight = props => props.tight && css`
+  padding: 0;
+`;
+
 const ContentCard = styled.div`
   padding: 1rem 1rem 2rem;
+  ${isTight}
   border-radius: 3px;
   background-color: white;
   box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px;
   overflow: hidden;
+  position: relative;
   ${isColumn}
 `;
 
 ContentCard.propTypes = {
   column: PropTypes.bool,
+  tight: PropTypes.bool,
 };
 
 export default ContentCard;
