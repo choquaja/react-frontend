0e5c119fbe125c0abc830a6c7322c0f157afdfbb
Ray Gesualdo <ray@rjgesualdo.com>
2017-06-03 17:22:21 -0400

    first pass at ServerDetails section
    Signed-off-by: Ray Gesualdo <ray@rjgesualdo.com>

--- src/scenes/Project/scenes/Resources/scenes/ServerDetails/index.js
+++ src/scenes/Project/scenes/Resources/scenes/ServerDetails/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs/dist/react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import Details from './scenes/Details';
+import Logs from './scenes/Logs';
 
 const DetailsWrapper = styled.div`
   margin-top: 2rem;
 `;
 
-export default function ServerDetails() {
+export default function ServerDetails({ server }) {
   return (
     <DetailsWrapper>
       <Tabs>
         <TabList>
           <Tab>Details</Tab>
-          <Tab>Environment Variables</Tab>
+          <Tab disabled>Environment Variables</Tab>
           <Tab disabled>SSH Tunnels</Tab>
-          <Tab>Data Sources</Tab>
+          <Tab disabled>Data Sources</Tab>
           <Tab>Logs</Tab>
         </TabList>
-        <TabPanel>View: Details</TabPanel>
-        <TabPanel>View: EnvVars</TabPanel>
-        <TabPanel>View: SSH</TabPanel>
-        <TabPanel>View: DataSources</TabPanel>
-        <TabPanel>View: Logs</TabPanel>
+        <TabPanel><Details server={server} /></TabPanel>
+        <TabPanel />
+        <TabPanel />
+        <TabPanel />
+        <TabPanel><Logs logsUrl={server.logs_url} /></TabPanel>
       </Tabs>
     </DetailsWrapper>
   );
 }
+
+ServerDetails.propTypes = {
+  server: PropTypes.object.isRequired,
+};
---
+++ src/scenes/Project/scenes/Resources/scenes/ServerDetails/scenes/Logs/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import AnimFade from '../../../../../../../../components/AnimFade';
+
+const LogList = styled.div`
+  padding: 1rem;
+  min-height: 10rem;
+  max-height: 40rem;
+  overflow-y: auto;
+  background: #182752;
+`;
+
+const LogLine = styled.div`
+  color: #fff;
+  font-size: .9em;
+  font-family: monospace;
+`;
+
+const renderLogLine = line => (
+  <AnimFade key={line}>
+    <LogLine key={line}>{line}</LogLine>
+  </AnimFade>
+);
+
+export default class Logs extends Component {
+  constructor(props) {
+    super(props);
+    try {
+      this.socket = new window.WebSocket(props.logsUrl);
+    } catch (err) {
+      // Do nothing eslint-disable-line
+    }
+  }
+
+  state = {
+    logs: [],
+  }
+
+  componentDidMount = () => {
+    if (this.socket) {
+      this.socket.addEventListener('message', (event) => {
+        console.log(event, event.data) // eslint-disable-line
+      });
+    }
+  }
+
+  componentWillUnmount = () => {
+    if (this.socket) this.socket.close();
+  }
+
+  render() {
+    return (
+      <LogList>{this.state.logs.map(renderLogLine)}</LogList>
+    );
+  }
+}
+
+Logs.propTypes = {
+  logsUrl: PropTypes.string.isRequired,
+};
