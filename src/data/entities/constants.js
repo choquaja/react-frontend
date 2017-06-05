746212a5e54817eca7cf22787bae6b4042116326
Ray Gesualdo <ray@rjgesualdo.com>
2017-06-05 18:36:58 -0400

    added actions/reducers to update and delete data entities
    Signed-off-by: Ray Gesualdo <ray@rjgesualdo.com>

--- src/data/entities/constants.js
+++ src/data/entities/constants.js
@@ -2,8 +2,12 @@ import { createAction } from '../../services/store/helpers';
 
 export const types = {
   UPDATE_ENTITIES: 'data.entities.UPDATE_ENTITIES',
+  UPDATE_ENTITY: 'data.entities.UPDATE_ENTITY',
+  DELETE_ENTITY: 'data.entities.DELETE_ENTITY',
 };
 
 export const actions = {
   updateEntities: createAction(types.UPDATE_ENTITIES),
+  updateEntity: createAction(types.UPDATE_ENTITY),
+  deleteEntity: createAction(types.DELETE_ENTITY),
 };
--- src/data/entities/reducer.js
+++ src/data/entities/reducer.js
@@ -1,9 +1,21 @@
 import { handleActions } from 'redux-actions';
 import merge from 'lodash/merge';
+import omit from 'lodash/omit';
+import update from 'lodash/update';
 import { types } from './constants';
+
+const mergeEntity = newData => oldData => merge({}, oldData, newData);
 
 export default handleActions({
   [types.UPDATE_ENTITIES](state, action) {
     return merge({}, state, action.payload);
+  },
+  [types.UPDATE_ENTITY](state, action) {
+    const { entityType, id, data } = action.payload;
+    return update({ ...state }, `${entityType}.${id}`, mergeEntity(data));
+  },
+  [types.DELETE_ENTITY](state, action) {
+    const { entityType, id } = action.payload;
+    return omit(state, `${entityType}.${id}`);
   },
 }, {});
