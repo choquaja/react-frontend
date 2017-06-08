bca4d74a4145c22bb2b484ed7e359339d8af30bc
Ray Gesualdo <rgesualdo@3blades.io>
2017-06-08 18:33:19 -0400

    added code to remove resource from Resource grid

--- src/scenes/Project/scenes/Resources/constants.js
+++ src/scenes/Project/scenes/Resources/constants.js
@@ -5,6 +5,7 @@ export const types = {
   GET_SERVERS_SUCCESS: 'data.projects.resources.GET_SERVERS_SUCCESS',
   GET_SERVERS_FAIL: 'data.projects.resources.GET_SERVERS_FAIL',
   UPDATE_SELECTED: 'data.projects.resources.UPDATE_SELECTED',
+  DELETE_RESOURCE: 'data.projects.resources.DELETE_RESOURCE',
 };
 
 export const actions = {
@@ -12,4 +13,5 @@ export const actions = {
   getServersSuccess: createAction(types.GET_SERVERS_SUCCESS),
   getServersFail: createAction(types.GET_SERVERS_FAIL),
   updateSelected: createAction(types.UPDATE_SELECTED),
+  deleteResource: createAction(types.DELETE_RESOURCE),
 };
--- src/scenes/Project/scenes/Resources/reducer.js
+++ src/scenes/Project/scenes/Resources/reducer.js
@@ -3,6 +3,10 @@ import { types } from './constants';
 import logic from './logic';
 import serverActionsLogic from './scenes/ServerActions/logic';
 
+const filterIfArray = predicate => data => (
+  Array.isArray(data) ? data.filter(predicate) : data
+);
+
 export default createDataReducer(
   types.GET_SERVERS_REQUEST,
   types.GET_SERVERS_SUCCESS,
@@ -12,6 +16,17 @@ export default createDataReducer(
     const selected = (!Array.isArray(action.payload) ? [action.payload] : action.payload);
     return {
       ...state,
+      selected,
+    };
+  },
+  [types.DELETE_RESOURCE](state, action) {
+    const filterData = filterIfArray(x => x !== action.payload);
+    const filterSelected = filterIfArray(x => x.id !== action.payload);
+    const data = filterData(state.data);
+    const selected = filterSelected(state.selected);
+    return {
+      ...state,
+      data,
       selected,
     };
   },
