1cff2d25c5460d177fe8ba014601c4c9e3776828
Ray Gesualdo <rgesualdo@3blades.io>
2017-06-09 16:59:22 -0400

    cleaned up Account scenes

--- src/scenes/Account/scenes/SSHKeys/index.js
+++ src/scenes/Account/scenes/SSHKeys/index.js
@@ -16,26 +16,13 @@ const Column2 = styled.div`
   width: 70%;
 `;
 
-const ButtonImg = styled(Button)`
-  background-color: #f2994a;
-  color: white;
-  border: none;
-  font-family: inherit;
-  font-size: 1.6rem;
-  height: 3.6rem;
-  font-weight: 700;
-  letter-spacing: normal;
-  padding: .5em 1.25em;
-  text-align: center;
-`;
-
 export default function Profile() {
   return (
     <Container>
       <Column2>
         <Title>SSH Keys</Title>
         <h2>SSH Public Key</h2>
-        <h5><ButtonImg>Reset SSH Key</ButtonImg></h5>
+        <Button primary block>Reset SSH Key</Button>
       </Column2>
     </Container>
   );
