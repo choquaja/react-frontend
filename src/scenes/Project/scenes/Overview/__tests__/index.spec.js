a58a3ac1e3165339d853a3b285ffaedd678cef40
Ray Gesualdo <rgesualdo@3blades.io>
2017-06-12 00:10:10 -0400

    updated tests

--- src/scenes/Project/scenes/Overview/__tests__/index.spec.js
+++ src/scenes/Project/scenes/Overview/__tests__/index.spec.js
@@ -4,37 +4,22 @@ import { Overview } from '../';
 
 describe('Overview', () => {
   it('renders without crashing', () => {
-    const props = {
-      loading: false,
-    };
-    shallow(<Overview {...props} />);
-  });
-
-  it('renders loading indicator', () => {
-    const props = {
-      loading: true,
-      data: null,
-    };
-    const component = shallow(<Overview {...props} />);
-    expect(component).toMatchSnapshot();
+    shallow(<Overview />);
   });
 
   it('renders empty content message', () => {
     const props = {
-      loading: false,
       data: null,
     };
     const component = shallow(<Overview {...props} />);
     expect(component).toMatchSnapshot();
   });
 
-  it('renders data regardless of loading', () => {
+  it('always renders data regardless', () => {
     const props = {
       data: { content: '# Sample Title' },
     };
-    const component1 = shallow(<Overview {...props} loading={false} />);
-    expect(component1).toMatchSnapshot();
-    const component2 = shallow(<Overview {...props} loading />);
-    expect(component2).toMatchSnapshot();
+    const component = shallow(<Overview {...props} />);
+    expect(component).toMatchSnapshot();
   });
 });
