35d9de45d92d49df63b4c56e4c98e7eab3d2f45f
Ray Gesualdo <rgesualdo@3blades.io>
2017-06-27 16:50:20 -0400

    changed private/public project icons for clarity

--- src/scenes/Home/components/NameLinkCell/index.js
+++ src/scenes/Home/components/NameLinkCell/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
-import { MdLockOpen, MdLockOutline } from 'react-icons/lib/md';
+// import { MdLockOpen, MdLockOutline } from 'react-icons/lib/md';
+import { TiLockClosedOutline, TiLockOpenOutline } from 'react-icons/lib/ti';
 import { Link } from 'react-router-dom';
 
-const PublicIcon = styled(MdLockOpen)`
+const PublicIcon = styled(TiLockOpenOutline)`
   margin-right: .5rem;
 `;
-const PrivateIcon = styled(MdLockOutline)`
+const PrivateIcon = styled(TiLockClosedOutline)`
   margin-right: .5rem;
 `;
+const ICON_SIZE = 24;
 
 const NameLinkCellComponent = props => (
   <span>
-    {props.isPrivate ? <PrivateIcon /> : <PublicIcon />}
+    {props.isPrivate ? <PrivateIcon size={ICON_SIZE} /> : <PublicIcon size={ICON_SIZE} />}
     <Link to={`/foo/${props.value}/overview`}>{props.value}</Link>
   </span>
 );
