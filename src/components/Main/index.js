af2589b66c9bcc16835618dbac283eb01a0c1213
Ray Gesualdo <rgesualdo@3blades.io>
2017-06-30 18:03:54 -0400

    reconfigured store service and added addLogic export

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,15 +3,13 @@ import { Provider } from 'react-redux';
 import { Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import { configureStore, INITIAL_STATE, history } from '../../services/store';
+import { store, history } from '../../services/store';
 import { theme } from '../../services/theme';
 import '../../services/theme/global';
 
 import Auth from '../../scenes/Auth';
 import Protected from './components/Protected';
 import ProtectedRoute from './components/ProtectedRoute';
-
-const store = configureStore(INITIAL_STATE);
 
 function Main() {
   return (
