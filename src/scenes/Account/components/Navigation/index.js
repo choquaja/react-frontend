decdb6e45122fa032a706d345692f975a3f1613c
Ray Gesualdo <rgesualdo@3blades.io>
2017-06-30 15:30:12 -0400

    cleaned up Account scene

---
+++ src/scenes/Account/components/Navigation/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+import {
+  MdPerson,
+  MdSettingsApplications,
+  // MdEmail,
+  MdVpnKey,
+  MdCompareArrows,
+} from 'react-icons/lib/md';
+import VerticalNavLink from '../../../../components/VerticalNavLink';
+
+const iconStyles = css`
+  margin-right: .8rem;
+`;
+
+const ProfileIcon = styled(MdPerson)`${iconStyles}`;
+const SettingsIcon = styled(MdSettingsApplications)`${iconStyles}`;
+// const EmailsIcon = styled(MdEmail)`${iconStyles}`;
+const SshKeysIcon = styled(MdVpnKey)`${iconStyles}`;
+const IntegrationsIcon = styled(MdCompareArrows)`${iconStyles}`;
+
+function Navigation({ url }) {
+  return (
+    <div>
+      <VerticalNavLink to={`${url}/profile`}>
+        <ProfileIcon size={20} />
+        Profile
+      </VerticalNavLink>
+      <VerticalNavLink to={`${url}/settings`}>
+        <SettingsIcon size={20} />
+        Settings
+      </VerticalNavLink>
+      {/* <VerticalNavLink to={`${url}/email`}>
+        <EmailsIcon size={20} />
+        Emails
+      </VerticalNavLink> */}
+      <VerticalNavLink to={`${url}/sshkeys`}>
+        <SshKeysIcon size={20} />
+        SSH-Key
+      </VerticalNavLink>
+      <VerticalNavLink to={`${url}/integrations`}>
+        <IntegrationsIcon size={20} />
+        Integrations
+      </VerticalNavLink>
+    </div>
+  );
+}
+
+Navigation.propTypes = {
+  url: PropTypes.string.isRequired,
+};
+
+export default Navigation;
--- src/scenes/Account/index.js
+++ src/scenes/Account/index.js
@@ -1,51 +1,47 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import styled from 'styled-components';
-import Navigation from './Navigation';
+
+import Row from '../../components/shared/row';
+import Column from '../../components/shared/column';
+import AnimFade from '../../components/AnimFade';
+import ContentCard from '../../components/ContentCard';
+import Navigation from './components/Navigation';
+
 import Profile from './scenes/Profile';
 import Settings from './scenes/Settings';
-import Email from './scenes/Email';
+// import Email from './scenes/Email';
 import SSHKeys from './scenes/SSHKeys';
 import Integrations from './scenes/Integrations';
 import Invite from './scenes/Invite';
 
-const Title = styled.h1`
-  font-size: 2em;
-`;
-
-const Container = styled.div`
-  display: flex;
-`;
-
-const Column1 = styled.div`
-  width: 20%;
-`;
-
 export default function Account(props) {
-  const { params, url } = props.match;
+  const { url } = props.match;
   return (
-    <Container>
-      <Column1>
-        <Title>Username</Title>
-        <Navigation {...params} />
-      </Column1>
-      <Switch>
-        <Redirect from={`${url}`} to={`${url}/profile`} exact />
-        <Route path={`${url}/profile`} component={Profile} />
-        <Route path={`${url}/settings`} component={Settings} />
-        <Route path={`${url}/email`} component={Email} />
-        <Route path={`${url}/sshkeys`} component={SSHKeys} />
-        <Route path={`${url}/integrations`} component={Integrations} />
-        <Route path={`${url}/invite`} component={Invite} />
-      </Switch>
-    </Container>
+    <AnimFade>
+      <div key="div">
+        <Row>
+          <Column size={3}>
+            <ContentCard column>
+              <Navigation url={url} />
+            </ContentCard>
+          </Column>
+          <Column size={9}>
+            <Switch>
+              <Redirect from={`${url}`} to={`${url}/profile`} exact />
+              <Route path={`${url}/profile`} component={Profile} />
+              <Route path={`${url}/settings`} component={Settings} />
+              {/* <Route path={`${url}/email`} component={Email} /> */}
+              <Route path={`${url}/sshkeys`} component={SSHKeys} />
+              <Route path={`${url}/integrations`} component={Integrations} />
+              <Route path={`${url}/invite`} component={Invite} />
+            </Switch>
+          </Column>
+        </Row>
+      </div>
+    </AnimFade>
   );
 }
-
-Account.defaultProps = {
-
-};
 
 Account.propTypes = {
   match: PropTypes.object.isRequired,
