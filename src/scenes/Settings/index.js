373f207998b3e3ab2b7aff5ab673b60190fa6711
Ray Gesualdo <rgesualdo@3blades.io>
2017-06-30 21:10:02 -0400

    swapped scene names for clarity

--- src/scenes/Settings/index.js
+++ src/scenes/Settings/index.js
@@ -9,13 +9,13 @@ import ContentCard from '../../components/ContentCard';
 import Navigation from './components/Navigation';
 
 import Profile from './scenes/Profile';
-import Settings from './scenes/Settings';
+import Account from './scenes/Account';
 // import Email from './scenes/Email';
 import SSHKeys from './scenes/SSHKeys';
 import Integrations from './scenes/Integrations';
 import Invite from './scenes/Invite';
 
-export default function Account(props) {
+export default function Settings(props) {
   const { url } = props.match;
   return (
     <AnimFade>
@@ -30,7 +30,7 @@ export default function Account(props) {
             <Switch>
               <Redirect from={`${url}`} to={`${url}/profile`} exact />
               <Route path={`${url}/profile`} component={Profile} />
-              <Route path={`${url}/settings`} component={Settings} />
+              <Route path={`${url}/account`} component={Account} />
               {/* <Route path={`${url}/email`} component={Email} /> */}
               <Route path={`${url}/sshkeys`} component={SSHKeys} />
               <Route path={`${url}/integrations`} component={Integrations} />
@@ -43,6 +43,6 @@ export default function Account(props) {
   );
 }
 
-Account.propTypes = {
+Settings.propTypes = {
   match: PropTypes.object.isRequired,
 };
